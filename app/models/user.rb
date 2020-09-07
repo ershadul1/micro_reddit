92c67c0059990fa61f820e6cf96d6c5ecb48a173
RNtaate <royhyde14@gmail.com>
2020-09-07 16:51:03 +0300

    Update README file and fix rubocop linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :name,  presence: true, length: { in: 2..20 }, uniqueness: true
+  validates :name, presence: true, length: { in: 2..20 }, uniqueness: true
   has_many :posts
   has_many :comments
 end
